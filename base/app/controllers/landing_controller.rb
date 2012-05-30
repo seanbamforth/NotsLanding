903dc50a203270febad322d3dfeec62ed1c3ade0
Sean Bamforth <sean@theguru.co.uk>
2012-05-30 13:33:09 +0100

    testing of partials

--- base/app/controllers/landing_controller.rb
+++ base/app/controllers/landing_controller.rb
@@ -3,5 +3,17 @@ class LandingController < ApplicationController
   end
 
   def home
+    #s = ""
+    #s += render_to_string :partial => "menu.html.erb"
+    #s += render_to_string :partial => "menu.html.erb"
+    #render :text => s
+
+    #section :menu
+    #section :main  
+    #section :welcome
+    #section :video
+
+    #section :features
+    #section :pricing , pricing=>price_list
   end
 end
